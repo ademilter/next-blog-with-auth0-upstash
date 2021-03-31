b994f58516211cd6015f5e447d42d719925b297d
Adem ilter <ademilter@gmail.com>
2021-03-31 22:51:32 +0300

    video-4

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import '../styles/global.css'
 import Header from '../components/header'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="antialiased text-gray-700">
-      <Header />
-      <main className="mt-6 mb-20">
-        <Component {...pageProps} />
-      </main>
-    </div>
+    <Auth0Provider
+      domain="adem.us.auth0.com"
+      clientId="lHCTe1mYPPQJwU1odCYaUEafbSSMtmRf"
+      redirectUri={process.env.NEXT_PUBLIC_URL}
+    >
+      <div className="antialiased text-gray-700">
+        <Header />
+        <main className="mt-6 mb-20">
+          <Component {...pageProps} />
+        </main>
+      </div>
+    </Auth0Provider>
   )
 }
 
--- pages/blog/[...slug].js
+++ pages/blog/[...slug].js
@@ -1,8 +1,11 @@
 import { getMdxNode, getMdxPaths } from 'next-mdx/server'
 import { useHydrate } from 'next-mdx/client'
 import { mdxComponents } from '../../components/mdx-components'
+import { useAuth0 } from '@auth0/auth0-react'
 
 export default function PostPage({ post }) {
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0()
+
   const content = useHydrate(post, {
     components: mdxComponents
   })
@@ -16,6 +19,41 @@ export default function PostPage({ post }) {
 
         <div className="prose">{content}</div>
       </article>
+
+      <form className="mt-10">
+        <textarea
+          rows="2"
+          className="border border-gray-300 rounded w-full block px-2 py-1"
+        />
+
+        <div className="mt-4">
+          {isAuthenticated ? (
+            <div className="flex items-center space-x-2">
+              <button className="bg-blue-600 text-white px-2 py-1 rounded">
+                Send
+              </button>
+              <img src={user.picture} width={30} className="rounded-full" />
+              <span>{user.name}</span>
+              <button
+                typeof="button"
+                onClick={() =>
+                  logout({ returnTo: process.env.NEXT_PUBLIC_URL + '/blog' })
+                }
+              >
+                x
+              </button>
+            </div>
+          ) : (
+            <button
+              className="bg-blue-600 text-white px-2 py-1 rounded"
+              typeof="button"
+              onClick={() => loginWithRedirect()}
+            >
+              Login
+            </button>
+          )}
+        </div>
+      </form>
     </div>
   )
 }
